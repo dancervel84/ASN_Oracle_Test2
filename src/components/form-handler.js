83487c5f06593f44bfb5450ebc31d75b273086d8
dancervel84 <dancervel84@gmail.com>
2025-06-11 09:47:01 -0600

    Updated form-handler.js

--- src/components/form-handler.js
+++ src/components/form-handler.js
@@ -17,7 +17,7 @@ export class FormHandler {
         this.addItemBtn.addEventListener('click', () => this.addLineItem());
         this.form.addEventListener('submit', (e) => this.handleSubmit(e));
         this.sendAppointmentBtn.addEventListener('click', (e) => this.handleAppointmentSubmit(e));
-        
+        send-appointment-btn.disable = true
         // Add initial line item
         this.addLineItem();
     }
