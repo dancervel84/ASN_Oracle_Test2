68608f5026a83aa3391dc0006d7eb165619cda7b
dancervel84 <dancervel84@gmail.com>
2025-06-11 10:43:33 -0600

    Updated dashboard-template.js

--- src/components/dashboard-template.js
+++ src/components/dashboard-template.js
@@ -112,7 +112,7 @@ export function createDashboardHTML(user) {
                                 </div>
                             </div>
                         </div>
-
+/**/
                        
                         </div>
                     </div>
