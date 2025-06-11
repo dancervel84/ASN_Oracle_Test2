b56d94ef7a7a350a95a6c32d58a5eb3aa79b3f5c
dancervel84 <dancervel84@gmail.com>
2025-06-11 10:38:37 -0600

    Updated dashboard-template.js

--- src/components/dashboard-template.js
+++ src/components/dashboard-template.js
@@ -113,7 +113,7 @@ export function createDashboardHTML(user) {
                             </div>
                         </div>
 
-                        /*<!-- Reports Module -->
+                        <!-- Reports Module -->
                         <div class="bg-white overflow-hidden shadow-lg rounded-lg hover:shadow-xl transition-shadow duration-300">
                             <div class="p-6">
                                 <div class="flex items-center">
@@ -135,7 +135,7 @@ export function createDashboardHTML(user) {
                                     </button>
                                 </div>
                             </div>
-                        </div>*/
+                        </div>
                     </div>
                 </div>
             </div>
