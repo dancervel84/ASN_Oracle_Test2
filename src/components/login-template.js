67037b912f6adde9b1e53efdad65ad91ada9704c
dancervel84 <dancervel84@gmail.com>
2025-06-11 10:47:05 -0600

    Updated login-template.js

--- src/components/login-template.js
+++ src/components/login-template.js
@@ -8,7 +8,7 @@ export function createLoginHTML() {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                         </svg>
                     </div>
-                    <h2 class="text-3xl font-bold text-gray-900">Sistema WMS</h2>
+                    <h2 class="text-3xl font-bold text-gray-900">Alimentar WMS</h2>
                     <p class="mt-2 text-sm text-gray-600">Ingrese sus credenciales para acceder</p>
                 </div>
                 
