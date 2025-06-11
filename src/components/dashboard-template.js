cfd67be80f56d497a642b5e167d6ca9841641cfb
dancervel84 <dancervel84@gmail.com>
2025-06-11 10:28:45 -0600

    Updated dashboard-template.js

--- src/components/dashboard-template.js
+++ src/components/dashboard-template.js
@@ -11,7 +11,7 @@ export function createDashboardHTML(user) {
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
                                 </svg>
                             </div>
-                            <h1 class="text-xl font-semibold text-gray-900">Sistema WMS</h1>
+                            <h1 class="text-xl font-semibold text-gray-900">Ingresar Datos WMS-CICE</h1>
                         </div>
                         
                         <div class="flex items-center space-x-4">
